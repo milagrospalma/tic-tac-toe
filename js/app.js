cee8cc7609b2e295412f66a7e5cd1ec1549b66a1
milagrospalma <mnpalma.2@gmail.com>
2017-11-08 10:41:30 -0500

    Agrega estructura html, estilos y archivo js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  var board = document.querySelector('.board-js');
+  board.addEventListener('click',addX);
+}
+
+var centinel = true;
+
+function addX(event){
+
+  if (event.target.matches('td') && event.target.textContent === '') {
+    if (centinel) {
+      event.target.textContent = 'X';
+    } else {
+      event.target.textContent = 'O';
+    }
+    centinel = !centinel;
+    }
+}
